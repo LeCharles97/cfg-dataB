428239e0b0bb0e478ec9885f4e7c1663efc7cf53
LeCharles97 <warcf1997@gmail.com>
2023-03-10 18:13:03 -0600

    resizing chunk

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/cfg-dataB/'
+  base : '/cfg-dataB/',
+  build: {
+    chunkSizeWarningLimit: 9000,
+  }
 })
