0a907bd2f068fa6dd8b0da350a23e3b315194683
LeCharles97 <warcf1997@gmail.com>
2023-03-10 20:55:01 -0600

    update: index.html

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
   Tools,
   FooterHome,
   About,
+  Form,
   Services,
 } from "./Components/index";
 
--- src/Components/index.js
+++ src/Components/index.js
@@ -3,6 +3,7 @@ import { Navbar } from "./Navbar";
 import { Tools } from "./Tools";
 import { FooterHome } from "./Footer";
 import { About } from "./About";
+import { Form } from "./Form";
 import { Services } from "./Services";
 
-export { HomeImage, Navbar, Tools, FooterHome, About,  Services };
+export { HomeImage, Navbar, Tools, FooterHome, About, Form, Services };
